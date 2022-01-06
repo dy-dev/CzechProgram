f3970a5b6360f80f2399fad57ea3292490692304
dy-dev <dominique.yolin@free.fr>
2022-01-06 16:30:45 +0100

    Exercise 1 on variable display and affectation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int x = 42;
+        String name = "Hello World";
+
+        System.out.println("First variable (x) : " + x +
+                ",\nand second variable (y) : " + name);
+        //x = name; Not possible
+        //name = x; Not possible
+        //Those 2 operation are not possible on their own, you need to call
+        //other methods to be able to achieve that
+
     }
+
+
 }
---
+++ src/com/company/SimpleClass.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class SimpleClass {
+    public int x;
+}
